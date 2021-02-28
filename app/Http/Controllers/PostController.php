fdcd0e710642697009c66beb479ed5db0dd34163
Rhea Marstaller <r.marstaller@gmail.com>
2021-02-28 17:37:33 +0100

    Don't give eye pain to Niclas.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -33,19 +33,19 @@ class PostController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      */
     public function store(PostRequest $request)
     {
-        auth()->user()->posts()->create($request->all());
+        $post = auth()->user()->posts()->create($request->all());
 
-        return redirect('category/' . $request->get('category_id'))->withStatus('Post successfully created.');
+        return redirect()->route('category.show', $post->category->id)->withStatus('Post successfully created.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Post  $post
+     * @param \App\Models\Post $post
      * @return \Illuminate\Http\Response
      */
     public function show(Post $post)
@@ -56,7 +56,7 @@ class PostController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Post  $post
+     * @param \App\Models\Post $post
      * @return \Illuminate\Http\Response
      */
     public function edit(Post $post)
@@ -67,8 +67,8 @@ class PostController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
+     * @param \Illuminate\Http\Request $request
+     * @param \App\Models\Post $post
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Post $post)
@@ -79,7 +79,7 @@ class PostController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Post  $post
+     * @param \App\Models\Post $post
      * @return \Illuminate\Http\Response
      */
     public function destroy(Post $post)
